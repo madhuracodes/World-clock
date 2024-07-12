fc0f39ff8cbc615ebf1d9a1196b3fe1a972d41be
madhuracodes <madhura84@gmail.com>
2024-07-12 20:38:49 +0200

    tweaked JS

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-setInterval(function () {
+function updateTime() {
   let berlinElement = document.querySelector("#berlin");
   let berlinDateElement = berlinElement.querySelector(".date");
   let berlinTimeElement = berlinElement.querySelector(".time");
   berlinDateElement.innerHTML = moment().format("ddd, LL");
   berlinTimeElement.innerHTML = moment().tz("Europe/Berlin").format("LTS");
-}, 1000);
 
-setInterval(function () {
   let mumbaiElement = document.querySelector("#mumbai");
   let mumbaiDateElement = mumbaiElement.querySelector(".date");
   let mumbaiTimeElement = mumbaiElement.querySelector(".time");
   mumbaiDateElement.innerHTML = moment().format("ddd, LL");
   mumbaiTimeElement.innerHTML = moment().tz("Asia/Calcutta").format("LTS");
-}, 1000);
+}
+
+updateTime();
+setInterval(updateTime, 1000);

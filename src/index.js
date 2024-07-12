410988a188c718a0a9aa9417e3b0b1b71805ffcb
madhuracodes <madhura84@gmail.com>
2024-07-12 10:35:08 +0200

    added moments and JS

---
+++ src/index.js
@@ -0,0 +1,11 @@
+let berlinElement = document.querySelector("#berlin");
+let berlinDateElement = berlinElement.querySelector(".date");
+let berlinTimeElement = berlinElement.querySelector(".time");
+berlinDateElement.innerHTML = moment().format("ddd, LL");
+berlinTimeElement.innerHTML = moment().tz("Europe/Berlin").format("LTS");
+
+let mumbaiElement = document.querySelector("#mumbai");
+let mumbaiDateElement = mumbaiElement.querySelector(".date");
+let mumbaiTimeElement = mumbaiElement.querySelector(".time");
+mumbaiDateElement.innerHTML = moment().format("ddd, LL");
+mumbaiTimeElement.innerHTML = moment().tz("Asia/Calcutta").format("LT");

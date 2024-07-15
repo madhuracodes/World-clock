51163f056ca911798d94c829c5df930301495372
madhuracodes <madhura84@gmail.com>
2024-07-15 12:12:03 +0200

    updated js to multiple location

--- src/index.js
+++ src/index.js
@@ -17,17 +17,21 @@ setInterval(updateTime, 1000);
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+
   let cityTime = moment().tz(cityTimeZone);
+  let cityName = cityTimeZone.replace("_", "").split("/")[1];
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `<div class="city">
           <div>
-            <h2>${cityTimeZone}</h2>
+            <h2>${cityName}</h2>
             <div class="date">${cityTime.format("ddd,LL")}</div>
           </div>
-          <div class="time">${cityTime.format("LTS")}</div>
+          <div class="time">${cityTime.format("LT")}</div>
         </div>
       `;
 }
-
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);

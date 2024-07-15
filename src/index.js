93eca668f277901043a981aee2755f5fc2819451
madhuracodes <madhura84@gmail.com>
2024-07-15 10:32:18 +0200

    added JS to dropdown

--- src/index.js
+++ src/index.js
@@ -14,3 +14,20 @@ function updateTime() {
 
 updateTime();
 setInterval(updateTime, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city">
+          <div>
+            <h2>${cityTimeZone}</h2>
+            <div class="date">${cityTime.format("ddd,LL")}</div>
+          </div>
+          <div class="time">${cityTime.format("LTS")}</div>
+        </div>
+      `;
+}
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
